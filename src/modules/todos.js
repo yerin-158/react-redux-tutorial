a0b3e98a2dada6cd0cc8a1e206a2504d1858e0a9
yerin-158 <hanblueblue@gmail.com>
2020-10-21 16:17:46 +0900

    redux-actions 이용하여 액션 생성함수, 리듀서 수정

--- src/modules/todos.js
+++ src/modules/todos.js
@@ -1,32 +1,24 @@
+import {createAction, handleActions} from 'redux-actions';
+
 const CHANGE_INPUT = 'todos/CHANGE_INPUT';
 const INSERT = 'todos/INSERT';
 const TOGGLE = 'todos/TOGGLE';
 const REMOVE = 'todos/REMOVE';
 
-export const changeInput = input => ({
-    type: CHANGE_INPUT,
-    input // 참고할 값. reducer 함수 내에서 사용할 수 있다. ex) action.input
-});
+export const changeInput = createAction(CHANGE_INPUT, input => input);
+// {type: 'CHANGE_INPUT', payload: ${input} }
 
 let id = 3; // insert 호출 될 때 마다 1씩 더해짐
-export const insert = text => ({
-    type: INSERT,
-    todo: {
-        id: id++,
-        text,
-        done: false
-    }
-});
+export const insert = createAction(INSERT, text => ({
+    id: id++,
+    text,
+    done: false
+}))
+// {type: 'INSERT', payload: {id: id++, text: ${text}, done:false} }
 
-export const toggle = id => ({
-    type: TOGGLE,
-    id
-})
+export const toggle = createAction(TOGGLE, id => id);
 
-export const remove = id => ({
-    type: REMOVE,
-    id
-});
+export const remove = createAction(REMOVE, id => id);
 
 const init = {
     input: '',
@@ -44,33 +36,29 @@ const init = {
     ]
 }
 
-const todos = (state=init, action) => {
-    switch (action.type) {
-        case CHANGE_INPUT:
-            return {
-                ...state,
-                input : action.input
-            };
-        case INSERT:
-            return {
-                ...state,
-                todos: state.todos.concat(action.todo)
-            };
-        case TOGGLE:
-            return {
-                ...state,
-                todos: state.todos.map(todo => 
-                    todo.id === action.id ? { ...todo, done: !todo.done } : todo
-                )
-            };
-        case REMOVE:
-            return {
-                ...state,
-                todos: state.todos.filter(todo => todo.id !== action.id)
-            }
-        default:
-            return state;
-    }
-}
+const todos = handleActions(
+    {
+        // payload를 input이라는 이름으로 할당.
+        [CHANGE_INPUT]: (state, {payload: input}) => ({
+            ...state, 
+            input
+        }),
+        [INSERT]: (state, {payload: todo}) => ({
+            ...state, 
+            todos: state.todos.concat(todo)
+        }),
+        [TOGGLE]: (state, {payload: id}) => ({
+            ...state, 
+            todos: state.todos.map(todo => 
+                todo.id === id ? { ...todo, done: !todo.done } : todo
+            )
+        }),
+        [REMOVE]: (state, {payload: id}) => ({
+            ...state,
+            todos: state.todos.filter(todo => todo.id !== id)
+        })
+    },
+    init
+);
 
 export default todos;
